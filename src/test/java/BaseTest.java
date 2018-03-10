7901d4763726de491fb40ac1d383dd31bcf10c41
OlgaLevitskaya <levitskaya-olga@yandex.ru>
2018-03-10 18:17:42 +0300

    BaseTest task1

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,27 @@
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseTest {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public static void beforeClass() {
+        driver = new ChromeDriver();
+        Assert.assertNotNull(driver);
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void testDriver() {
+        driver.get("https://yandex.ru");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        driver.quit();
+        driver = null;
+    }
+}
